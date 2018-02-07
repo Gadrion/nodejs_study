1acd1265c865e661640b917085819f1f5432e4bb
gadrion <wjdrlehs57@gmail.com>
2018-02-07 17:52:02 +0900

    2일차  - aws에서 .pem파일 생성  - puttygen으로 .pkg 파일 생성  - putty로 ubuntu 연결  - aws의 마리아 DB 연결

--- 2day/NewExpressProject/app.js
+++ 2day/NewExpressProject/app.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
 var users = require('./routes/users');
+var main = require('./routes/main');
 
 var app = express();
 
@@ -25,6 +26,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 // 작업 단위 혹은 성격에 따라 카테고리를 나눈다 (path를 분류)
 app.use('/', index);
 app.use('/users', users);
+app.use('/main', main);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
