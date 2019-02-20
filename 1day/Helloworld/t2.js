1999f778bff6d32f7bef2421a52bcd6459a59a5b
gd <wjdrlehs57@gmail.com>
2019-02-20 21:31:52 +0900

    [study] all delete

--- 1day/Helloworld/t2.js
+++
@@ -1,8 +0,0 @@
-/*
- *  모듈 가져오기 및 사용
- */
-
-var t1 = require('./t1.js');
-console.log( t1 );
-console.log( t1.name2 );
-console.log( t1.getAge(15) );
--- 2day/NewExpressProject/reboot.sh
+++
@@ -1,1 +0,0 @@
-sudo /usr/bin/forerver restart ./bin/www
